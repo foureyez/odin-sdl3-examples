c446e59b2457aed19cd143112b67947fc166bd0f
Abhijeet Rawat <abhirawat1304@gmail.com>
2025-05-22 11:41:58 +0530

    update shader compilation

--- compile_shaders.sh
+++ compile_shaders.sh
@@ -1,6 +1,10 @@
 mkdir -p ./assets/shaders/compiled
 glslang ./assets/shaders/triangle.frag.glsl -V -o ./assets/shaders/compiled/triangle.frag.spv
 glslang ./assets/shaders/triangle.vert.glsl -V -o ./assets/shaders/compiled/triangle.vert.spv
+shadercross ./assets/shaders/compiled/triangle.vert.spv -s SPIRV -d MSL -o ./assets/shaders/compiled/triangle.vert.msl
+shadercross ./assets/shaders/compiled/triangle.frag.spv -s SPIRV -d MSL -o ./assets/shaders/compiled/triangle.frag.msl
 
 glslang ./assets/shaders/quad.frag.glsl -V -o ./assets/shaders/compiled/quad.frag.spv
 glslang ./assets/shaders/quad.vert.glsl -V -o ./assets/shaders/compiled/quad.vert.spv
+shadercross ./assets/shaders/compiled/quad.vert.spv -s SPIRV -d MSL -o ./assets/shaders/compiled/quad.vert.msl
+shadercross ./assets/shaders/compiled/quad.frag.spv -s SPIRV -d MSL -o ./assets/shaders/compiled/quad.frag.msl
